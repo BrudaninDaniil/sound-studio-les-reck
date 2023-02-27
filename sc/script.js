9751a4c5ebc6c60f01d10176598e62ac00f71a18
Danil Brudanin <brudankovko@gmail.com>
2023-02-27 23:43:29 +0300

    new version

---
+++ sc/script.js
@@ -0,0 +1,8 @@
+const documentElement = document.documentElement
+console.log(documentElement)
+const documentBody = document.body
+console.log(documentBody)
+console.log(documentBody.firstChild)
+const ChildNodes = documentBody.childNodes
+console.log(ChildNodes)
+console.log(documentBody.children)
